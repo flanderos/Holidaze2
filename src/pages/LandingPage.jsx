ebd3f8d5bc1b96039af214abee0d0a52c08932c8
Anders Hellerud <andershellerud@hotmail.no>
2024-11-29 22:26:58 +0100

    added a hardcoded review section

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import TopVenues from "../components/TopVenuesForLandingPage";
+import Reviews from "../components/Reviews";
 
 const LandingPage = () => {
   return (
@@ -9,6 +10,7 @@ const LandingPage = () => {
       <Header title="Holidaze" />
       <HeroSection />
       <TopVenues />
+      <Reviews />
     </div>
   );
 };
