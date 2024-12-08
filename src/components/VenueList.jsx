c73ee875b11b65e4687309b52ada0deddc3fed07
Anders Hellerud <andershellerud@hotmail.no>
2024-12-08 21:13:30 +0100

    responsive now, must fix header before i can make it more responsive

--- src/components/VenueList.jsx
+++ src/components/VenueList.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const MainContainer = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 20px;
   padding: 20px;
   align-items: flex-start; /* Sørger for at innholdet justeres fra toppen */
