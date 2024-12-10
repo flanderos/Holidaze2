c2b12e470b6ecd8c9219db1e12a9661c8923256b
Anders Hellerud <andershellerud@hotmail.no>
2024-12-10 22:09:33 +0100

    cleaned up folder structure and fixed link to all venues

---
+++ src/components/LinkToAllVenues.jsx
@@ -0,0 +1,30 @@
+/* import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+
+const StyledLink = styled(Link)`
+  height: 50px;
+  width: 250px;
+  color: #fff;
+  background-color: black;
+  border-radius: 25px;
+  border: 1px solid white;
+  font-family: "Michroma", sans-serif;
+  transition: 0.6s;
+  display: block;
+  margin: 0 auto;
+  margin-top: 50px;
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 13px 0px;
+  }
+`;
+
+const AllVenuesButton = () => {
+  return <StyledLink to={"/VenuesPage"}>See all Venues</StyledLink>;
+};
+
+export default AllVenuesButton;
+ */
---
+++ src/components/globalcomponents/StyledLink.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const StyledLink = styled(Link)`
+  color: #fff;
+  max-width: 250px;
+  padding: 15px;
+  background-color: ${({ bgColor }) => bgColor || "black"};
+  border-radius: 25px;
+  font-family: "Michroma", sans-serif;
+  transition: 0.6s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  margin: 0 auto;
+  margin-top: 50px;
+  font-family: montserrat;
+
+  &:hover {
+    cursor: pointer;
+    box-shadow:
+      rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+      rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  }
+`;
